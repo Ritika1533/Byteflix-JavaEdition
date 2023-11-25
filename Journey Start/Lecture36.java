9fecbc02f37f074be0c00ba5f819e2fc62720a9f
unknown <ritikakes33@gmail.com>
2023-11-25 16:34:56 +0530

    learning

---
+++ Journey Start/Lecture36.java
@@ -0,0 +1,7 @@
+package javacourse;
+
+public class Lecture36 {
+    public static void main(String[] args){
+        System.out.println("oops concept");
+    }
+}
---
+++ Journey Start/Lecture37.java
@@ -0,0 +1,7 @@
+package javacourse;
+
+public class Lecture37 {
+    public static void main(String[] args){
+        System.out.println("oops concept");
+    }
+}
